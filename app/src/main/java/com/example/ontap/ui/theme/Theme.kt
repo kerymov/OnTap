edd165e14f120575ff6b836d71c4384b14dab50d
Ilya Kerymov <illya.kerymov31@gmail.com>
2022-05-26 02:58:45 +0300

    Customize system bars.

--- app/src/main/java/com/example/ontap/ui/theme/Theme.kt
+++ app/src/main/java/com/example/ontap/ui/theme/Theme.kt
@@ -3,6 +3,7 @@ package com.example.ontap.ui.theme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.runtime.Composable
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorPalette = darkColors(
     primary = Floral,
@@ -17,6 +18,11 @@ private val DarkColorPalette = darkColors(
 
 @Composable
 fun OnTapTheme(content: @Composable () -> Unit) {
+    val systemUiController = rememberSystemUiController()
+
+    systemUiController.setStatusBarColor(DarkColorPalette.background, false)
+    systemUiController.setNavigationBarColor(DarkColorPalette.primary, false)
+
     MaterialTheme(
         colors = DarkColorPalette,
         typography = Typography,
